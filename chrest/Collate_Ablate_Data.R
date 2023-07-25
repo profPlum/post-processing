76c636b7424cfbf1186ff9b28f9bdf47afda3a73
Dwyer Deighan <dwyerdei@buffalo.edu>
2023-07-24 20:14:11 -0400

    commiting fixed version of Collate_Ablate_Data.R

--- chrest/Collate_Ablate_Data.R
+++ chrest/Collate_Ablate_Data.R
@@ -1,7 +1,10 @@
-library(tidyverse)
+#library(tidyverse)
+library(readr)
+library(purrr)
 
 #input_dir = '/Users/dwyerdeighan/CLionProjects/ablate/ablateInputs/chemTabDiffusionFlame/_1DSampleDiffusionFlame/flowField_mixtureFraction/'
-input_dir = commandArgs(trailingOnly=T)[[1]] # like: sys.argv
-all_files = input_dir |> list.files('.*\\.csv\\.gz') %>% file.path(input_dir, .)
+#input_dir = commandArgs(trailingOnly=T)[[1]] # like: sys.argv
+#all_files = input_dir |> list.files('.*\\.csv\\.gz') %>% file.path(input_dir, .)
+all_files = commandArgs(trailingOnly=T) # like: sys.argv
 master_df = all_files |> map_dfr(read_csv)
-write.csv(master_df, file='ablate_collated.csv', row.names = F)
+write.csv(master_df, file=gzfile('ablate_collated.csv.gz'), row.names = F)
