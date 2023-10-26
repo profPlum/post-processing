b85dd5eb2064304346172bce64a53ec829ec2a8c
Dwyer Deighan <dwyerdei@buffalo.edu>
2023-10-26 17:08:35 -0400

    added more elegant booleans & an automation to automatically select the most recent simulation run for collation

--- chrest/process_single_ablate_file.sh
+++ chrest/process_single_ablate_file.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-#should_rebalance=T # set this flag to T or "" (empty)
-#chemtab_output=T # should be passed as an argument!
-
 echo first arg: $1
-echo should_rebalance: $should_rebalance
-
 final_output="${1%.*}.csv.gz"
 intermediate_csv="$final_output"
 
 #final_output="$intermediate_csv"
-if [ $should_rebalance ]; then
+if ((should_rebalance)); then
     final_output="${1%.*}-rebalanced.csv.gz"
 fi
 
 echo final_output: $final_output
 
-if [ -e "$final_output" ]; then 
+if [[ -e "$final_output" ]]; then
     echo exiting, final output already exists
     return 0 || exit 0 &> /dev/null
 fi
@@ -24,9 +19,10 @@ echo proceeding! with input hdf5: $1
 
 echo checking if we have chemtab output...
 fields_list="monitor_energySource:souener monitor_Yi:Yi aux_temperature:temp"
-if [ "$chemtab_output" ]; then
+if [[ $chemtab_output ]]; then
     echo answer: True
-    fields_list="$fields_list monitor_progressSource:soucpv"
+    fields_list="$fields_list aux_Progress:mass_ monitor_progressSource:source_"
+    # results in: mass_CPV_PC* & source_CPV_PC*, verified 10/4/23
 else
     echo answer: False
     fields_list="$fields_list monitor_yiSource:souspec monitor_zMix:zmix"
@@ -36,8 +32,10 @@ fi
 base_1="$(basename $1)"
 echo base_1: ${base_1#*.}
 domain_file="$(dirname $1)/../domain/domain.${base_1#*.}"
-if [ $should_rebalance ]; then
+if ((should_rebalance)); then
     should_rebalance=--rebalance-flame
+else
+    should_rebalance=
 fi
 
 python wrangle_ablate_dataset_for_ChemTab.py $should_rebalance --file "$1" "$domain_file" --fields $fields_list #--n-cubes-per-dim 25
