cfa4f3a3e550e77db7f6b6b7d1dd9c4435930cdc
Dwyer Deighan <dwyerdei@buffalo.edu>
2023-11-03 14:13:31 -0400

    added change to Collate_Ablate_Data.R which sorts rather than shuffles collated data b/c it seems shuffling can easily be handled later in ChemtabUQ but sorting is better preprocessing step

--- chrest/Collate_Ablate_Data.R
+++ chrest/Collate_Ablate_Data.R
@@ -1,9 +1,6 @@
 library(tidyverse)
 
-#input_dir = '/Users/dwyerdeighan/CLionProjects/ablate/ablateInputs/chemTabDiffusionFlame/_1DSampleDiffusionFlame/flowField_mixtureFraction/'
-#input_dir = commandArgs(trailingOnly=T)[[1]] # like: sys.argv
-#all_files = input_dir |> list.files('.*[0-9]{5}\\.csv\\.gz') %>% file.path(input_dir, .)
 all_files = commandArgs(trailingOnly=T) # like: sys.argv
 master_df = all_files %>% sort() %>% map2_dfr(., 1:len(.), ~read_csv(.x) |> mutate(time_key=.y)) %>%
-  slice_sample(prop=1) # we are shuffling here b/c deterministic splitting will happen later for multiprocessing safeness
+    arrange(time, x, y, z) # %>% slice_sample(prop=1) # we are shuffling here b/c deterministic splitting will happen later for multiprocessing safeness
 write.csv(master_df, file=gzfile('ablate_collated.csv.gz'), row.names = F)
